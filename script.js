71f02863a839efb34f75f475b19848f5f41e7e29
Rojo98 <166548352+Rojo98-OverCode@users.noreply.github.com>
2026-01-17 18:44:35 -0400

    actualizacion del js

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-function abrirWhatsapp() {
+function abrirWhatsapp(){
     const telefono = "59172866357";
-    const mensaje = "Hola, deseo una cotización de minerales.";
-    window.open(`https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`, "_blank");
+    const mensaje = "Hola, deseo información y cotización de minerales.";
+    window.open(
+        `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`,
+        "_blank"
+    );
 }
 
-function mostrarInfo(mineral) {
-    alert(
-        "Información del mineral:\n\n" +
-        mineral +
-        "\n\nPara más detalles y cotización, contáctanos por WhatsApp o correo."
-    );
+function mostrarInfo(mineral){
+    const archivos = {
+        "Sal": "Ficha técnica SAL GRANULADA.pdf",
+        "Puzolana": "Ficha técnica PUZOLANA.pdf",
+        "Baritina": "Ficha técnica BARITINA.pdf",
+        "Bentonita": "Ficha técnica BENTONITA.pdf",
+        "Rodio": "FICHA TECNICA RODIO.pdf",
+        "Caolín": "Ficha técnica KAOLIN.pdf"
+    };
+
+    const archivo = archivos[mineral];
+
+    if(archivo){
+        const enlace = document.createElement("a");
+        enlace.href = `pdf/${archivo}`;
+        enlace.download = archivo;
+        document.body.appendChild(enlace);
+        enlace.click();
+        document.body.removeChild(enlace);
+    }
 }
