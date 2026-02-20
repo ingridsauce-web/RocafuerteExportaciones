9852f15562e2e675d09b3a595edc6dd8fabc1ce7
Ingrid Sauce <ingridsauce@gmail.com>
2026-02-20 10:29:24 -0400

    responsivo

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const toggle = document.getElementById("menu-toggle");
+const nav = document.getElementById("nav");
+
+toggle.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
 function abrirWhatsapp(){
     window.open("https://wa.me/59172866357","_blank")
 }
